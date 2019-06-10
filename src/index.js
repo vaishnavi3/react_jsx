939281fd7782f2af5ab3d78ec60ef8de78202612
Vaishnavi <vaishnavi.bhaarde@blimp.co.in>
2019-06-10 17:14:27 +0530

    example to use jsx in react

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
+
+// import react , react-dom  libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// create react component
+const App = function() {
+  const buttonText = 'Click me!';
+  return (
+    <div>
+      <label className="label" htmlFor="name">Enter name: </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: 'blue', color: 'black' }}>{buttonText}</button>
+    </div>
+  )
+};
+ReactDOM.render(
+    <App />,
+    document.querySelector("#root")
+);
